7a9f53edb7b159228e7b3b6c5a768f96db56c985
socratest3 <socratest3@nycstudents.net>
2025-04-11 21:34:53 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,1 +1,22 @@
+public class Book
+{
+    /** The title of the book */
+    private String title;
 
+    /** The price of the book */
+    private double price;
+
+    /** Creates a new Book with given title and price */
+    public Book(String bookTitle, double bookPrice){
+        title = bookTitle;
+        price = bookPrice;
+    }
+    public String getTitle()
+    {   return title;  }
+
+    /** Returns a string containing the title and price of the Book */
+    public String getBookInfo()
+    {
+        return title + "-" + price;
+    }
+}
