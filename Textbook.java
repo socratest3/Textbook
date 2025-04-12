24c9dda9b392aa1e7e8ed2e49acdeec4d6fe11d9
socratest3 <socratest3@nycstudents.net>
2025-04-11 21:35:39 -0400

    Update Textbook.java

--- Textbook.java
+++ Textbook.java
@@ -1,1 +1,25 @@
+public class Textbook extends Book
+{
+    private int edition;
+    public Textbook(String title, double price, int edition )
+    {
+        super(title, price);
+        this.edition = edition;
+    }
 
+    public int getEdition()
+    {
+        return edition;
+    }
+
+    public String getBookInfo()
+    {
+        return super.getBookInfo() + "-" + edition;
+    }
+    
+    public boolean canSubstituteFor(Textbook other)
+    {
+     if(getTitle().equals(other.getTitle())){return edition>=other.edition;}
+return false;
+    }
+}
